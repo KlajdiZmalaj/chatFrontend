950e2a778456391baa43a9d1839e6a1de41db26d
Klajdi Zmalaj <32370399+KlajdiZmalaj@users.noreply.github.com>
2021-04-23 18:12:44 +0200

    Updates

---
+++ precache-manifest.2ef6dfe0bcd9df6683c0c29070e0e057.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f53781c89b33be0382ae68385178d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26631b5ee3574094d4a1",
+    "url": "/static/css/2.d54708ad.chunk.css"
+  },
+  {
+    "revision": "7a42ea24a0cf70a89a04",
+    "url": "/static/css/main.cb30d273.chunk.css"
+  },
+  {
+    "revision": "26631b5ee3574094d4a1",
+    "url": "/static/js/2.999866fb.chunk.js"
+  },
+  {
+    "revision": "47348ea03924e02b6bece9d830ef99d9",
+    "url": "/static/js/2.999866fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a42ea24a0cf70a89a04",
+    "url": "/static/js/main.865a7643.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "3516ee3a2ee8e228b0a33b7dcbefe14a",
+    "url": "/static/media/2592543.3516ee3a.jpg"
+  }
+]);
