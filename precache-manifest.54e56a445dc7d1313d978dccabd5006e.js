9218072904364a585dfadecd262c7dc64f34ca2a
Klajdi Zmalaj <klajdizmalaj@icloud.com>
2021-05-25 18:54:50 +0200

    Updates

---
+++ precache-manifest.54e56a445dc7d1313d978dccabd5006e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943a735d00d52a10b908620d7eb4ae17",
+    "url": "/chatFrontend/index.html"
+  },
+  {
+    "revision": "088550b6fb96124587f5",
+    "url": "/chatFrontend/static/css/2.d54708ad.chunk.css"
+  },
+  {
+    "revision": "91479efd3ef44c1100a8",
+    "url": "/chatFrontend/static/css/main.fa8c792d.chunk.css"
+  },
+  {
+    "revision": "088550b6fb96124587f5",
+    "url": "/chatFrontend/static/js/2.0c3c27e8.chunk.js"
+  },
+  {
+    "revision": "47348ea03924e02b6bece9d830ef99d9",
+    "url": "/chatFrontend/static/js/2.0c3c27e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91479efd3ef44c1100a8",
+    "url": "/chatFrontend/static/js/main.6c29cf2a.chunk.js"
+  },
+  {
+    "revision": "f374d3115625c02e99ee",
+    "url": "/chatFrontend/static/js/runtime-main.e66eb65f.js"
+  },
+  {
+    "revision": "3516ee3a2ee8e228b0a33b7dcbefe14a",
+    "url": "/chatFrontend/static/media/2592543.3516ee3a.jpg"
+  }
+]);
