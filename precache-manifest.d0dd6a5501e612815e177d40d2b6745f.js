4e52cd36b27389b4b335106adc19654c1f229fe6
Klajdi Zmalaj <frontendaccessibilityexpert@phoenix.me.uk>
2021-12-13 15:11:12 +0100

    Updates

--- precache-manifest.d0dd6a5501e612815e177d40d2b6745f.js
+++ precache-manifest.d0dd6a5501e612815e177d40d2b6745f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a040a2da3ee6ab3425983c09e3dd950d",
+    "revision": "6c1e4a6d0a5df5eb9a2d5faa1c145149",
     "url": "/chatFrontend/index.html"
   },
   {
-    "revision": "e6accace45a21e31fe30",
+    "revision": "012b66e8e8986f215902",
     "url": "/chatFrontend/static/css/2.d54708ad.chunk.css"
   },
   {
-    "revision": "068d52fa560c9753ef58",
-    "url": "/chatFrontend/static/css/main.61c975a4.chunk.css"
+    "revision": "d9ec1c4e053c34b56eb6",
+    "url": "/chatFrontend/static/css/main.f0aacfd4.chunk.css"
   },
   {
-    "revision": "e6accace45a21e31fe30",
-    "url": "/chatFrontend/static/js/2.544b48f2.chunk.js"
+    "revision": "012b66e8e8986f215902",
+    "url": "/chatFrontend/static/js/2.d50ceac4.chunk.js"
   },
   {
     "revision": "47348ea03924e02b6bece9d830ef99d9",
-    "url": "/chatFrontend/static/js/2.544b48f2.chunk.js.LICENSE.txt"
+    "url": "/chatFrontend/static/js/2.d50ceac4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "068d52fa560c9753ef58",
-    "url": "/chatFrontend/static/js/main.91822aac.chunk.js"
+    "revision": "d9ec1c4e053c34b56eb6",
+    "url": "/chatFrontend/static/js/main.8a2caa38.chunk.js"
   },
   {
     "revision": "f374d3115625c02e99ee",
